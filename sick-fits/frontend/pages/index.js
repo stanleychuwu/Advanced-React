76da6336cad2d068087ff541c186a4a595dbe726
Stanley Wu <stanley@Stanleys-MacBook-Pro.local>
2023-08-28 11:11:06 -0700

    remove imported Page component from index.js and sell.js

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,11 +1,9 @@
-import Page from '../components/Page';
-
 export default function IndexPage() {
   return (
-    <Page cool="Heck ya!">
+    <>
       <p>Hello!</p>
       <code>var</code>
       <p>I am a child!</p>
-    </Page>
+    </>
   );
 }
