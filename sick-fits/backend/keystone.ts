c6b444f97e70d220c318d963b5286b0a1a44acb5
Stanley Wu <stanley@Stanleys-MBP.attlocal.net>
2023-09-08 12:22:23 -0700

    add config for keystone

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,3 +1,4 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 
 const databaseURL =
@@ -8,3 +9,24 @@ const sessionConfig {
     secret: process.env.COOKIE_SECRET,
 };
 
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      creditials: true,
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO, add session values here
+});
