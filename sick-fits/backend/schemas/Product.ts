a33d892188d38b290db383fc1ca35fc1e2cd43b5
Stanley Wu <stanley@Stanleys-MBP.attlocal.net>
2023-09-18 14:53:57 -0700

    Add Product schema status and price

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -1,5 +1,5 @@
 import { list } from '@keystone-next/keystone/schema';
-import { text } from '@keystone-next/fields';
+import { integer, select, text } from '@keystone-next/fields';
 
 export const Product = list({
   // TODO
@@ -11,5 +11,19 @@ export const Product = list({
         displayMode: 'textarea',
       },
     }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+    // TODO: Photo
   },
 });
