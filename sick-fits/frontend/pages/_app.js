56754edf827cfb1f80b64c4e1984ad8915d3f8bd
Stanley Wu <stanley@Stanleys-MBP.attlocal.net>
2023-10-11 14:13:17 -0700

    Added way to grab queries upon App load for NextJS

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -20,4 +20,13 @@ function MyApp({ Component, pageProps, apollo }) {
   );
 }
 
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
 export default withData(MyApp);
