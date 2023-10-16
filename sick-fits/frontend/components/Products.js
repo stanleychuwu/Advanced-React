37d2a64446e76e71dd6c6467e0a2646d7fabbd61
Stanley Wu <stanley@Stanleys-MBP.attlocal.net>
2023-10-16 13:23:34 -0700

    add list of products query

---
+++ sick-fits/frontend/components/Products.js
@@ -0,0 +1,28 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log(data, error, loading);
+  return (
+    <div>
+      <p>Products!!</p>
+    </div>
+  );
+}
