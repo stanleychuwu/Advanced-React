5487d8188414cdb54db505ee310e0abd4d8bf94c
Stanley Wu <stanley@Stanleys-MBP.attlocal.net>
2023-10-23 17:15:28 -0700

    Add price to product

--- sick-fits/frontend/components/Product.js
+++ sick-fits/frontend/components/Product.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
 
 export default function Product({ product }) {
   return (
@@ -12,6 +14,9 @@ export default function Product({ product }) {
       <Title>
         <Link href={`/product/${product.id}`}>{product.name}</Link>
       </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      {/* TODO: Add buttons to edit and delete item */}
     </ItemStyles>
   );
 }
